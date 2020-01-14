4ebf07058ed9be1f7a2e82804aaba55772f2f495
Thomas Brouard <contact@brrd.fr>
2020-01-14 12:20:45 +0100

    Fix error handling in export.js

--- src/actions/export.js
+++ src/actions/export.js
@@ -22,56 +22,51 @@ async function writePDF(destPath, contents) {
 }
 
 async function doExport({ template, sourceFile }, options) {
-	try {
-		// Get config dir
-		const configDir = utils.getPath().configDir;
-		if (!configDir) {
-			throw Error('Harpagon project not found.');
-		}
-
-		// Read config
-		const configPath = path.join(configDir, 'config.yml');
-		const yamlConfig = await readFile(configPath, 'utf8');
-		const config = yaml.safeLoad(yamlConfig);
-
-		// Find template
-		// Use .harpagon/templates/ in priority, otherwise use default templates/ dir
-		const userTemplatePath = path.join(configDir, 'templates', template + '.njk');
-		
-		const userTemplateExists = await checkFileExists(userTemplatePath)
-		const templateDir = userTemplateExists ? 
-			path.resolve(userTemplatePath, '..') :
-			path.join(require.main.filename, '../../templates');
-
-		// Set nunjucks temlates path
-		nunjucks.configure(templateDir);
-		
-		// Create data object
-		const yamlData = await readFile(sourceFile, 'utf8');
-		const data = yaml.safeLoad(yamlData);
-		data._config = config;
-
-		// JSON => HTML
-		const html = nunjucks.render(template + '.njk', data);
-
-		if (options.format && options.format.toLowerCase() === 'html') {
-			// Export HTML
-			const destPath = path.join(path.dirname(sourceFile), path.basename(sourceFile, path.extname(sourceFile)) + '-' + template + '.html');
-			await writeFile(destPath, html, 'utf8');
-			return;
-		} 
-
-		// HTML => PDF
-		const destPath = path.join(path.dirname(sourceFile), path.basename(sourceFile, path.extname(sourceFile)) + '-' + template + '.pdf');
-		await writePDF(destPath, html);
+	// Get config dir
+	const configDir = utils.getPath().configDir;
+	if (!configDir) {
+		throw Error('Harpagon project not found.');
 	}
-	catch (e) {
-		console.error(e);
-	}
+
+	// Read config
+	const configPath = path.join(configDir, 'config.yml');
+	const yamlConfig = await readFile(configPath, 'utf8');
+	const config = yaml.safeLoad(yamlConfig);
+
+	// Find template
+	// Use .harpagon/templates/ in priority, otherwise use default templates/ dir
+	const userTemplatePath = path.join(configDir, 'templates', template + '.njk');
+	
+	const userTemplateExists = await checkFileExists(userTemplatePath)
+	const templateDir = userTemplateExists ? 
+		path.resolve(userTemplatePath, '..') :
+		path.join(require.main.filename, '../../templates');
+
+	// Set nunjucks temlates path
+	nunjucks.configure(templateDir);
+	
+	// Create data object
+	const yamlData = await readFile(sourceFile, 'utf8');
+	const data = yaml.safeLoad(yamlData);
+	data._config = config;
+
+	// JSON => HTML
+	const html = nunjucks.render(template + '.njk', data);
+
+	if (options.format && options.format.toLowerCase() === 'html') {
+		// Export HTML
+		const destPath = path.join(path.dirname(sourceFile), path.basename(sourceFile, path.extname(sourceFile)) + '-' + template + '.html');
+		await writeFile(destPath, html, 'utf8');
+		return;
+	} 
+
+	// HTML => PDF
+	const destPath = path.join(path.dirname(sourceFile), path.basename(sourceFile, path.extname(sourceFile)) + '-' + template + '.pdf');
+	await writePDF(destPath, html);
 }
 
 module.exports = function(args, options, logger) {
 	doExport(args, options)
 		.then(() => console.log('Document was successfully exported.'))
-		.catch(console.error);;
+		.catch(console.error);
 };
